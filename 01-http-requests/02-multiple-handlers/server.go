e1ebac3fb4e430f9f9dabb73c2d6d66238b9258a
j.lazerson <jake.lazerson@valuecentric.com>
2020-03-01 06:26:37 -0500

    BH - gwp  - thank you Hashem

--- 01-http-requests/02-multiple-handlers/server.go
+++ 01-http-requests/02-multiple-handlers/server.go
@@ -14,9 +14,33 @@ func world(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	// -- ------------------------------
+	/*
+			Defines parameters for running an HTTP server.
+
+			The Server struct configuration:
+
+		    type Server struct {
+		        Addr           string
+		        Handler        Handler
+		        ReadTimeout    time.Duration
+		        WriteTimeout   time.Duration
+		        MaxHeaderBytes int
+		        TLSConfig      *tls.Config
+		        TLSNextProto   map[string]func(*Server, *tls.Conn, Handler)
+		        ConnState      func(net.Conn, ConnState)
+		        ErrorLog       *log.Logger
+		    }
+
+
+			For details see: https://pkg.go.dev/net/http?tab=doc#Server
+
+	*/
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr: "127.0.0.1:8090",
 	}
+
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/world", world)
 
