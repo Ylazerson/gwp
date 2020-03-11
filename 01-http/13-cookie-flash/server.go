ec8619ed1a7e5d5187e2d7857a6dfe927d6a76e4
j.lazerson <jake.lazerson@valuecentric.com>
2020-03-11 14:22:47 -0400

    BH - gwp - thank you Hashem

--- 01-http/13-cookie-flash/server.go
+++ 01-http/13-cookie-flash/server.go
@@ -7,6 +7,21 @@ import (
 	"time"
 )
 
+/*
+// -- ------------------------------
+There are many ways to implement flash messages, but one
+of the most common is to store them in session cookies
+that are removed when the page is refreshed.
+
+Run:
+    http://localhost:8080/set_message
+	http://localhost:8080/show_message
+	http://localhost:8080/show_message
+	http://localhost:8080/show_message
+// -- ------------------------------
+*/
+
+// -- ------------------------------
 func setMessage(w http.ResponseWriter, r *http.Request) {
 	msg := []byte("Hello World!")
 	c := http.Cookie{
@@ -16,24 +31,43 @@ func setMessage(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &c)
 }
 
+// -- ------------------------------
 func showMessage(w http.ResponseWriter, r *http.Request) {
+
+	// -- --------------------------
+	// Get "flash" cookie:
 	c, err := r.Cookie("flash")
+
+	// -- --------------------------
 	if err != nil {
+
 		if err == http.ErrNoCookie {
 			fmt.Fprintln(w, "No message found")
 		}
+
 	} else {
+
+		// -- ----------------------
+		/*
+			Here you’re replacing the existing cookie, essentially removing
+			it altogether because the MaxAge field is a negative number and
+			the Expires field is in the past.
+		*/
 		rc := http.Cookie{
 			Name:    "flash",
 			MaxAge:  -1,
 			Expires: time.Unix(1, 0),
 		}
+
 		http.SetCookie(w, &rc)
 		val, _ := base64.URLEncoding.DecodeString(c.Value)
 		fmt.Fprintln(w, string(val))
+
 	}
+
 }
 
+// -- ------------------------------
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
