800638c2cca3ee451da67eabb4b6c34aa712cbac
j.lazerson <jake.lazerson@valuecentric.com>
2020-03-12 06:27:37 -0400

    BH - gwp - thank you Hashem

--- 03-storing-data/02-csv-files/store.go
+++ 03-storing-data/02-csv-files/store.go
@@ -7,20 +7,28 @@ import (
 	"strconv"
 )
 
+// -- ----------------------------------
 type Post struct {
 	Id      int
 	Content string
 	Author  string
 }
 
+// -- ----------------------------------
 func main() {
-	// creating a CSV file
+
+	// -- ------------------------------
+	// Create a CSV file:
+
 	csvFile, err := os.Create("posts.csv")
+
 	if err != nil {
 		panic(err)
 	}
+
 	defer csvFile.Close()
 
+	// -- ------------------------------
 	allPosts := []Post{
 		Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"},
 		Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"},
@@ -28,36 +36,71 @@ func main() {
 		Post{Id: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"},
 	}
 
+	// -- ------------------------------
+	// Write to CSV file:
 	writer := csv.NewWriter(csvFile)
+
 	for _, post := range allPosts {
-		line := []string{strconv.Itoa(post.Id), post.Content, post.Author}
+
+		line := []string{
+			strconv.Itoa(post.Id),
+			post.Content,
+			post.Author,
+		}
+
 		err := writer.Write(line)
+
 		if err != nil {
 			panic(err)
 		}
 	}
+
+	// Flush writes any buffered data to the underlying io.Writer.
 	writer.Flush()
 
-	// reading a CSV file
+	// -- ------------------------------
+	// Read a CSV file:
 	file, err := os.Open("posts.csv")
+
 	if err != nil {
 		panic(err)
 	}
+
 	defer file.Close()
 
+	// -- ------------------------------
+	/*
+		FieldsPerRecord
+			If negative number:
+				Indicates you aren’t that bothered if you don’t
+				have all the fields in the record.
+
+			If positive number:
+				The number of fields you expect from each record
+				and Go will throw an error if you get less.
+
+			0:
+				Use the number of fields in the first record as
+				the FieldsPerRecord value.
+	*/
 	reader := csv.NewReader(file)
 	reader.FieldsPerRecord = -1
+
 	record, err := reader.ReadAll()
+
 	if err != nil {
 		panic(err)
 	}
 
+	// -- ------------------------------
 	var posts []Post
+
 	for _, item := range record {
 		id, _ := strconv.ParseInt(item[0], 0, 0)
 		post := Post{Id: int(id), Content: item[1], Author: item[2]}
 		posts = append(posts, post)
 	}
+
 	fmt.Println(posts[0].Id)
 	fmt.Println(posts[0].Content)
 	fmt.Println(posts[0].Author)
