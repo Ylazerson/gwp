012a8a488b2770340377d4c78dc23104d26ece48
j.lazerson <jake.lazerson@valuecentric.com>
2020-03-15 08:06:28 -0400

    BH - gwp - thank you Hashem

--- 04-web-services/05-web-service/main.go
+++ 04-web-services/05-web-service/main.go
@@ -54,39 +54,73 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 // -- ---------------------------------------
-// Retrieve a post
+// Retrieve a single post.
 // GET /post/1
 func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
+
+	// -- -----------------------------------
+	/*
+		1. Extract the URL’s path
+		2. Get the id using the path.Base function.
+		3. The id is a string so convert it into an integer.
+	*/
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
+	// Get single post from DB and put into struct:
 	post, err := retrieve(id)
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
+	// Marshal the struct into JSON:
 	output, err := json.MarshalIndent(&post, "", "\t\t")
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
+	// Write JSON to the ResponseWriter:
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(output)
+
 	return
 }
 
 // -- ---------------------------------------
-// Create a post
+// Create a single post.
 // POST /post/
 func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
+
+	// -- -----------------------------------
+	// Create slice of bytes:
 	len := r.ContentLength
 	body := make([]byte, len)
+
+	// -- -----------------------------------
+	// Read request body into slice:
 	r.Body.Read(body)
+
+	// -- -----------------------------------
+	// Unmarshal JSON into struct:
 	var post Post
 	json.Unmarshal(body, &post)
+
+	// -- -----------------------------------
+	// Create DB record:
 	err = post.create()
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
 	w.WriteHeader(200)
 	return
 }
@@ -95,22 +129,35 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 // Update a post
 // PUT /post/1
 func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
+
+	// -- -----------------------------------
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
 	post, err := retrieve(id)
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
 	len := r.ContentLength
 	body := make([]byte, len)
 	r.Body.Read(body)
 	json.Unmarshal(body, &post)
+
+	// -- -----------------------------------
 	err = post.update()
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
 	w.WriteHeader(200)
 	return
 }
@@ -119,18 +166,29 @@ func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
 // Delete a post
 // DELETE /post/1
 func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
+
+	// -- -----------------------------------
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
 	post, err := retrieve(id)
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
 	err = post.delete()
+
 	if err != nil {
 		return
 	}
+
+	// -- -----------------------------------
 	w.WriteHeader(200)
 	return
 }
